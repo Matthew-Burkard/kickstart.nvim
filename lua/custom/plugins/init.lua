a40720a5be7c497e909d3af5027a7b58a4cb4eaf
Matthew <matthew@burkard.cloud>
2024-08-10 17:44:55 -0400

    feat: use prod fork of vim-doge

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,5 @@ return {
   { "LunarVim/bigfile.nvim" },
   { "mg979/vim-visual-multi" },
   { "ahmedkhalf/project.nvim" },
+  { "Matthew-Burkard/vim-doge" },
 }
---
+++ lua/custom/plugins/vimdoge.lua
@@ -0,0 +1,5 @@
+return {
+  -- "Matthew-Burkard/vim-doge",
+  -- dev = true,
+  -- dir = "~/Projects/OpenSource/vim-doge",
+}
