8df16abdbe09be283ce18264f6f3f343f6824c63
Matthew <matthew@burkard.cloud>
2024-08-26 21:03:58 -0400

    refactor: vim spellcheck is so bad it's worse than no spellcheck

--- init.lua
+++ init.lua
@@ -11,9 +11,6 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
--- Spell check.
-vim.opt.spell = true
-
 -- Make line numbers default
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
